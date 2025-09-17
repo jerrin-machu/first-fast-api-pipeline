5c8483b7aac80ebbfd47c6d037500bbd14c50b2d
Jerrin Thomas <jerrinmachu@gmail.com>
2025-09-17 10:04:11 +0530

    fix for wait for db script to exit cleanly

--- wait_for_db.py
+++ wait_for_db.py
@@ -1,7 +1,7 @@
-# wait_for_db.py
 import os
 from tenacity import retry, wait_fixed, stop_after_delay
 import psycopg2
+import sys
 
 
 @retry(wait=wait_fixed(2), stop=stop_after_delay(60))
@@ -12,16 +12,22 @@ def wait_for_db():
     password = os.getenv("POSTGRES_PASSWORD", "postgres")
     dbname = os.getenv("POSTGRES_DB", "postgres")
 
-
     print(f"Trying to connect to Postgres at {host}:{port} as {user}...")
-    conn = psycopg2.connect(host=host, port=port, user=user, password=password, dbname=dbname)
+    conn = psycopg2.connect(
+        host=host,
+        port=port,
+        user=user,
+        password=password,
+        dbname=dbname
+    )
     conn.close()
-    print("Connected to Postgres")
+    print("Connected to Postgres ✅")
 
 
 if __name__ == '__main__':
     try:
         wait_for_db()
+        sys.exit(0)   # exit cleanly after success
     except Exception as e:
-        print("Timed out waiting for Postgres:", e)
-    raise
+        print("❌ Timed out waiting for Postgres:", e)
+        sys.exit(1)
